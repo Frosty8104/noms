b23116692022ba5cf76326ead92d57cf2231519a
laura <lg8104@gmail.com>
2023-03-25 13:40:35 -0600

    Updating invitation page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var instagram = document.querySelector(".fa-instagram");
 instagram.style.color = "#ff6347";
 // Add background image
 var foodImage = document.querySelector(".food");
-foodImage.style.backgroundImage = "url('img/victoria-shes.unsplash.jpg')";
+foodImage.style.backgroundImage = "url('../img/victoria-shes.unsplash.jpg')";
 // Dotted border-bottom around h1
 var noms = document.querySelector("h1");
 noms.style.borderBottom = "4px dotted #ff6347";
